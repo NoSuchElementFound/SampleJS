0f4c3cc0224cb2bcd8087f52c450ccc3e9ce6bca
mearchit <74748787+mearchit@users.noreply.github.com>
2024-01-07 20:33:52 +0530

    Second Commit

--- TestARRAY.js
+++ TestARRAY.js
@@ -15,3 +15,70 @@ console.log(arr.join("-"))
 strcon=arr.concat(arr1)
 console.log(strcon)
 
+
+var names=["A","b","c","d","e","g"]
+names.splice(5,0,"f")
+console.log(names)
+names.splice(2,2)
+console.log(names)
+names.splice(4)
+console.log(names)
+names.sort()    //sort method is ok for string but it creates string for numbers and compare them letter by letter
+var num=[4,36,21,9,19]
+console.log(num.sort())
+
+
+// how to sort numbers using sort method
+num.sort(function (a,b){
+    return a-b
+})
+console.log(num)
+
+
+
+//Object Array
+var obj=[
+    {
+        name:"Archit",
+        age:22},
+    {
+        name:"Anshu",
+        age:23
+    },
+    {
+        name:"Arpit",
+        age:22
+    },
+    {
+        name:"Abhinav",
+        age:21
+    }]
+console.log(obj.sort(function(a,b){return a.age-b.age==0?a.name-b.name:a.age-b.age}))
+
+
+//for each loop
+numbers=[1,12,3,46,5,66]
+numbers.forEach(function (number)
+{
+    console.log(number)
+})
+
+//filter function
+var grt10=numbers.filter(function (num){
+    return num>10
+})
+console.log(grt10)
+
+//every function(compare all items present on the given condition)(gives boolean)
+var result=numbers.every(function (num){
+    return num>10
+})
+console.log(result)
+
+//some function(finds atleast one if available item to follow condition)
+var result1=numbers.some(function (num){
+    return num>10
+})
+console.log(result1)
+
+
