e489050109df1a97b4c3fae8fef59cd0d964441c
mearchit <74748787+mearchit@users.noreply.github.com>
2024-01-08 18:05:40 +0530

    Sixth

---
+++ TestMAPnSET.js
@@ -0,0 +1,20 @@
+//---------------------------------------------//
+//Map is a copy of obejct in JS
+var map=new Map()
+map.set("a",1)
+map.set("b",2)
+map.set("c",3)
+console.log(map)
+console.log(map.get("c"))
+
+
+
+//-----------------------------------------------//
+//Set is an copy of Array(but it doesn't allow duplicate elements)
+var set=new Set()
+set.add(1)
+set.add(2)
+set.add(3)
+set.add(4)
+set.add(3)
+console.log(set)
